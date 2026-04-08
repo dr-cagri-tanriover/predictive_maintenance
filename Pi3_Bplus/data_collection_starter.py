ce8360db16536a7871030a4a39c033c1f57fbba1
circuitapps <125308597+circuitapps@users.noreply.github.com>
2026-04-07 17:45:27 -0700

    Made corrections to json files. Added small test code to the new file.

--- Pi3_Bplus/data_collection_starter.py
+++ Pi3_Bplus/data_collection_starter.py
@@ -1,3 +1,12 @@
-
 import json
 
+if __name__ == "__main__":
+    
+    with open("./Pi3_Bplus/waveform_definitions.json", "r", encoding="utf-8") as f:
+        waveform_defs = json.load(f)
+
+    with open("./Pi3_Bplus/brake_definitions.json", "r", encoding="utf-8") as f:
+        brake_defs = json.load(f)
+
+    print(f"Waveform Definitions: {waveform_defs}")
+    print(f"Brake Definitions: {brake_defs}")
